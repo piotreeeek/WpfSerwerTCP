8faf5e76ab0eea8292b4007726ad60b26a0428b9
Piotrek <piotrek120017@gmail.com>
2017-11-16 16:30:50 +0100

    first step for ftp

--- WpfSerwerTCP/ClientServerTheard.cs
+++ WpfSerwerTCP/ClientServerTheard.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,6 +12,7 @@ namespace WpfSerwerTCP
         Socket clientSocket;
         bool running = true;
         byte[] messageByte = new byte[1024];
+        string path = "D:/politechnika/semestr_7/sieciowe/przykladowe";
 
         public ClientServerTheard(Socket s)
         {
@@ -33,6 +36,48 @@ namespace WpfSerwerTCP
                         this.running = false;
                         Console.WriteLine("Klient " + this.clientSocket.RemoteEndPoint.ToString() + " rozłaczony.");
                         Serwer.AddStatement("Klient " + this.clientSocket.RemoteEndPoint.ToString() + " rozłaczony.\n");
+                    }
+                    else if(message.ToUpper() == "LIST")
+                    {
+                        s.Send(sendByte);
+                        Console.WriteLine("Wiadomośc od klienta " + s.RemoteEndPoint.ToString() + " : " + message);
+                        Serwer.AddStatement("Wiadomośc od klienta " + s.RemoteEndPoint.ToString() + " : " + message + "\n");
+                        string[] entries = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
+                        string list = "";
+                        foreach(string entry in entries)
+                        {
+                            Console.WriteLine(Path.GetFileName(entry));
+                            list = list + Path.GetFileName(entry) + "\n";
+
+                        }
+
+                    }
+                    else if (message.Split(' ').First().ToUpper() == "SHOW")
+                    {
+                        s.Send(sendByte);
+                        Console.WriteLine("Wiadomośc od klienta " + s.RemoteEndPoint.ToString() + " : " + message);
+                        Serwer.AddStatement("Wiadomośc od klienta " + s.RemoteEndPoint.ToString() + " : " + message + "\n");
+                        string fileName = message.Split(' ')[1];
+
+                        if (File.Exists(path + "/" + fileName)) 
+                        {
+                            byte[] fileBytes = File.ReadAllBytes(path + "/" + fileName);
+                            StringBuilder sb = new StringBuilder();
+
+                            foreach (byte b in fileBytes)
+                            {
+                                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                            }
+                            Console.WriteLine(sb);
+                            //FileStream file = File.Open(path + fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                        }
+
+                        string[] entries = Directory.GetFiles("D:/politechnika/semestr_7/sieciowe/przykladowe", "*", SearchOption.TopDirectoryOnly);
+                        foreach (string entry in entries)
+                        {
+                            Console.WriteLine(Path.GetFileName(entry));
+                            Console.WriteLine("dupa");
+                        }
                     }
                     else
                     {
